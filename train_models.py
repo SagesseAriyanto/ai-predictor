e325a6db6c1102b216a4cf02ae7d63b6d269107e
Sagesse PC <sagesseariyanto@gmail.com>
2025-12-29 10:54:01 -0700

    added success column

--- train_models.py
+++ train_models.py
@@ -8,12 +8,12 @@ import pickle
 # Load dataset
 data = pd.read_csv("./ai_data.csv")
 
-# TODO: FUNCTION TO DETERMINE CATEGORY BASED ON DESCRIPTION
+# TODO: Model to determine AI company category based on description
 data_categories = data.dropna(
     subset=["Description", "Category"]
 )  # drop rows with missing Description or Category
 
-# Split data into features X and target y (y = f(X))
+# Split data into features X and target y (y = f(X))D
 X_desc = data_categories["Description"]  # feature column
 y_category = data_categories["Category"]  # target column
 
@@ -37,6 +37,13 @@ X_test_vec = vectorizer.transform(X_test)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train_vec, y_train)
 
-# Save the trained model and vectorizer (wb = write binary)
-pickle.dump(model, open("./Models/model_category.pkl", "wb"))
-pickle.dump(vectorizer, open("./Models/vectorizer_category.pkl", "wb"))
+# # Save the trained model and vectorizer (wb = write binary)
+# pickle.dump(model, open("./Models/model_category.pkl", "wb"))
+# pickle.dump(vectorizer, open("./Models/vectorizer_category.pkl", "wb"))
+
+# TODO: Models to determine success and upvotes for AI companies based on category
+
+# Create 'Success' column based on median upvotes per category
+data.dropna(inplace=True)       # drop NaN rows
+data['Category_median'] = data.groupby('Category')['Upvotes'].transform('median')
+data['Success'] = (data['Upvotes'] >= data['Category_median']).astype(int)
