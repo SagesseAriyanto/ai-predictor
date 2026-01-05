4be89e7ceaf61baabd207a0ca4d3e9b024b2ff63
Sagesse PC <sagesseariyanto@gmail.com>
2026-01-05 12:14:30 -0700

    working on cleaning data before sending to gemini model

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,22 @@
 from google import genai
+from dotenv import load_dotenv
+import pandas as pd
+import os
 
-# 1. Create the client (it will auto-detect 'GEMINI_API_KEY' from your environment)
-client = genai.Client(api_key="YOUR_API_KEY")
 
-# 2. Generate content (Note: 'models.generate_content' is the new syntax)
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Tell me a haiku about coding."
-)
+def get_resp(prompt):
+    load_dotenv()
 
-print(response.text)
+    # Read data from CSV
+    df = pd.read_csv('./ai_data.csv')
+    print(df.head())
+    print(df.info())
+    print(df['Price'].value_counts())
+    # client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+    # response = client.models.generate_content(
+    #     model="gemini-2.5-flash", contents=prompt
+    # )
+
+    # return response.text
+    
+get_resp("text")
