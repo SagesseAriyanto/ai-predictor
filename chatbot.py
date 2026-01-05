f5b7fcca32c9f08bead12efb6286539c916becf3
Sagesse PC <sagesseariyanto@gmail.com>
2026-01-05 12:23:00 -0700

    converted NaN prices and dropped Nan description

--- chatbot.py
+++ chatbot.py
@@ -9,9 +9,22 @@ def get_resp(prompt):
 
     # Read data from CSV
     df = pd.read_csv('./ai_data.csv')
-    print(df.head())
-    print(df.info())
-    print(df['Price'].value_counts())
+
+    # Map redundant price values to 'Free'
+    price_mapping = {
+        "GitHub": "Free",
+        "Open Source": "Free",
+        "Google Colab": "Free"
+    }
+    df['Price'] = df['Price'].replace(price_mapping)
+    print(df.isna().sum())
+
+    # Convert NaN prices to 'N/A'
+    df['Price'] = df['Price'].fillna('N/A')
+
+    df.dropna(inplace=True)
+    print(df.isna().sum())
+    
     # client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
     # response = client.models.generate_content(
     #     model="gemini-2.5-flash", contents=prompt
